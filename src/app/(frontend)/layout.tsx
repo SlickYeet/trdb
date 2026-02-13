c7f200a380b0bbaf7209b66e5cfd37be35ed59a8
SlickYeet <lasse@famlam.ca>
2026-02-12 22:34:07 -0800

    feat: update font variables and styles in layout and globals

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from "next"
+import { Geist_Mono, Noto_Sans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
 
 import "@/styles/globals.css"
+
+const notoSans = Noto_Sans({
+  variable: "--font-sans",
+})
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+})
 
 export const metadata: Metadata = {
   description: "The Recipe Database | by Lx2.dev",
@@ -10,7 +22,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html className="dark" lang="en">
-      <body className="font-sans antialiased">{children}</body>
+      <body
+        className={cn(
+          "font-mono antialiased",
+          notoSans.variable,
+          geistMono.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
