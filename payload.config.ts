3ca53a6942613154893cb2fd8db5fad8f60528bb
SlickYeet <lasse@famlam.ca>
2026-02-12 20:49:10 -0800

    feat: migrate database from SQLite to Postgres and update import map

--- payload.config.ts
+++ payload.config.ts
@@ -2,14 +2,14 @@
 
 import path from "node:path"
 import { fileURLToPath } from "node:url"
-import { sqliteAdapter } from "@payloadcms/db-sqlite"
-import { payloadCloudPlugin } from "@payloadcms/payload-cloud"
+import { postgresAdapter } from "@payloadcms/db-postgres"
 import { lexicalEditor } from "@payloadcms/richtext-lexical"
 import { buildConfig } from "payload"
 import sharp from "sharp"
 
 import { Media } from "@/collections/Media"
 import { Users } from "@/collections/Users"
+import { env } from "@/env"
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -22,20 +22,16 @@ export default buildConfig({
     user: Users.slug,
   },
   collections: [Users, Media],
-  db: sqliteAdapter({
-    // SQLite-specific arguments go here.
-    // `client.url` is required.
-    client: {
-      authToken: process.env.DATABASE_AUTH_TOKEN,
-      url: process.env.DATABASE_URL || "",
+  db: postgresAdapter({
+    pool: {
+      connectionString: env.DATABASE_URL,
     },
   }),
   editor: lexicalEditor(),
   plugins: [
-    payloadCloudPlugin(),
     // storage-adapter-placeholder
   ],
-  secret: process.env.PAYLOAD_SECRET || "",
+  secret: env.PAYLOAD_SECRET,
   sharp,
   typescript: {
     outputFile: path.resolve(dirname, "payload-types.ts"),
